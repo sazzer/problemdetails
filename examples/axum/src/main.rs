b55eca96cc5f250d5a5e7df734a8cc0ef84d5cef
Paweł Jan Czochański <pawel@czochanski.pl>
2024-02-01 17:16:30 +0100

    Add an example JSON rejection which returns a problemdetail response

--- examples/axum/src/main.rs
+++ examples/axum/src/main.rs
@@ -1,8 +1,17 @@
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
+use axum::{
+    extract::{
+        rejection::{self, BytesRejection, JsonRejection},
+        FromRequest,
+    },
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
 use http::StatusCode;
-use problemdetails::axum::PanicHandlerBuilder;
+use problemdetails::{axum::PanicHandlerBuilder, Problem};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
@@ -10,7 +19,8 @@ async fn main() {
     let app = Router::new()
         .route("/", get(notimplemented))
         .route("/forbidden", get(forbidden))
-        .route("/panic", get(panic));
+        .route("/panic", get(panic))
+        .route("/json", post(json));
 
     // add a panic handler if you want one
     let app = app.layer(
@@ -42,4 +52,56 @@ async fn forbidden() -> Result<String, problemdetails::Problem> {
 
 async fn panic() {
     panic!("Oh no!");
+}
+
+// example how to handle JSON rejection and return a problemdetail
+
+// some example JSON deserializable data
+#[derive(Debug, Deserialize)]
+struct ExampleData {
+    name: String,
+}
+
+// an example handler with our special MyJson extractor
+async fn json(MyJson(data): MyJson<ExampleData>) {
+    println!("{:?}", data);
+    println!("{}", data.name);
+}
+
+pub struct MyRejection(Problem);
+
+#[derive(FromRequest)]
+#[from_request(via(axum::Json), rejection(MyRejection))]
+pub struct MyJson<T>(pub T);
+
+impl IntoResponse for MyRejection {
+    fn into_response(self) -> axum::response::Response {
+        return self.0.into_response();
+    }
+}
+
+impl From<JsonRejection> for MyRejection {
+    fn from(rejection: rejection::JsonRejection) -> Self {
+        let (status, body, title) = match rejection {
+            JsonRejection::JsonDataError(err) => {
+                (err.status(), err.body_text(), "JSON deserialization error")
+            },
+            JsonRejection::JsonSyntaxError(err) => {
+                (err.status(), err.body_text(), "JSON syntax error")
+            },
+            JsonRejection::MissingJsonContentType(err) => {
+                (err.status(), err.body_text(), "Content type error")
+            },
+            JsonRejection::BytesRejection(BytesRejection::FailedToBufferBody(err)) => {
+                (err.status(), err.body_text(), "Request buffering error")
+            },
+            _ => (StatusCode::BAD_GATEWAY, "".to_string(), "Unknown error"),
+        };
+
+        MyRejection(
+            problemdetails::new(status)
+                .with_title(body)
+                .with_detail(title),
+        )
+    }
 }
